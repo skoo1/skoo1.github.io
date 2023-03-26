31ebe6d067f2594f88a2c31b446f3c8001ae6964
skoo <skoo@kaist.ac.kr>
2023-03-26 14:07:08 +0900

    wingnut test

--- three_js_01/main.js
+++ three_js_01/main.js
@@ -6,8 +6,12 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders
 const scene = new THREE.Scene()
 
 //Axes Show
-const axesHelper = new THREE.AxesHelper(20);
-scene.add( axesHelper );
+const axesWorld = new THREE.AxesHelper(20)
+scene.add(axesWorld)
+
+//Axes Show
+const axesBody = new THREE.AxesHelper(20)
+scene.add(axesBody)
 
 //Mesh Ball
 const geometry_ball = new THREE.SphereGeometry(1, 64, 64)
@@ -33,73 +37,102 @@ loader.load(
 )
 
 //Dynamics Calculations
-const htime = 0.01
+const htime = 0.001
 const NutIB = new THREE.Matrix3()
 NutIB.set(1, 0, 0,
           0, 2, 0,
           0, 0, 3)
 
-const Nutq0_world = new THREE.Quaternion()
-Nutq0_world.set(0, 0, 0, 1)
-const Nutw0_body = new THREE.Vector3()
-Nutw0_body.set(0, 0, 1)
+const Nut_q0_world = new THREE.Quaternion()
+Nut_q0_world.set(0.00999983333, 0, 0, 0.99995000041)
+const Nut_w0_body = new THREE.Vector3()
+Nut_w0_body.set(0, 1, 0)
 const NutTorque_body = new THREE.Vector3()
 NutTorque_body.set(0, 0, 0)
 
 const Nut_q_current_world = new THREE.Quaternion()
-Nut_q_current_world.copy(Nutq0_world)
+Nut_q_current_world.copy(Nut_q0_world)
 const Nut_q_current_body = new THREE.Quaternion()
 Nut_q_current_body.set(0, 0, 0, 1)
 const Nut_w_current_body = new THREE.Vector3()
-Nut_w_current_body.copy(Nutw0_body)
+Nut_w_current_body.copy(Nut_w0_body)
+const Nut_w_current_world = new THREE.Vector3()
+Nut_w_current_world.copy(Nut_w_current_body)
+Nut_w_current_world.applyQuaternion(Nut_q_current_world)
 
-// calculate w_croxx_IB_mult_w
-const Nut_Iw_current_body = new THREE.Vector3()
-Nut_Iw_current_body.copy(Nut_w_current_body)
-Nut_Iw_current_body.applyMatrix3(NutIB)
-const Nut_w_x_Iw_current_body = new THREE.Vector3()
-Nut_w_x_Iw_current_body.copy(Nut_w_current_body)
-Nut_w_x_Iw_current_body.cross(Nut_Iw_current_body)
+function update_Nut_pose() {
+    // calculate w_croxx_IB_mult_w
+    const Nut_Iw_current_body = new THREE.Vector3()
+    Nut_Iw_current_body.copy(Nut_w_current_body)
+    Nut_Iw_current_body.applyMatrix3(NutIB)
+    const Nut_w_x_Iw_current_body = new THREE.Vector3()
+    Nut_w_x_Iw_current_body.copy(Nut_w_current_body)
+    Nut_w_x_Iw_current_body.cross(Nut_Iw_current_body)
 
-// calculate w_dot_body = IBInv * (Torque - w_croxx_IB_mult_w)
-const Nut_Torque_sub_wIw = new THREE.Vector3()
-Nut_Torque_sub_wIw.copy(NutTorque_body)
-Nut_Torque_sub_wIw.sub(Nut_w_x_Iw_current_body)
-const NutIBInv = new THREE.Matrix3()
-NutIBInv.copy(NutIB)
-NutIBInv.invert()
-const Nut_w_dot_current_body = new THREE.Vector3()
-Nut_w_dot_current_body.copy(Nut_Torque_sub_wIw)
-Nut_w_dot_current_body.applyMatrix3(NutIBInv)
+    // calculate w_dot_body = IBInv * (Torque - w_croxx_IB_mult_w)
+    const Nut_Torque_sub_wIw = new THREE.Vector3()
+    Nut_Torque_sub_wIw.copy(NutTorque_body)
+    Nut_Torque_sub_wIw.sub(Nut_w_x_Iw_current_body)
+    const NutIBInv = new THREE.Matrix3()
+    NutIBInv.copy(NutIB)
+    NutIBInv.invert()
+    const Nut_w_dot_current_body = new THREE.Vector3()
+    Nut_w_dot_current_body.copy(Nut_Torque_sub_wIw)
+    Nut_w_dot_current_body.applyMatrix3(NutIBInv)
 
-var qx, qy, qz, qw, wx, wy, wz
-qx = Nut_q_current_body.x
-qy = Nut_q_current_body.y
-qz = Nut_q_current_body.z
-qw = Nut_q_current_body.w
-wx = Nut_w_current_body.x
-wy = Nut_w_current_body.y
-wz = Nut_w_current_body.z
+    var qx, qy, qz, qw, wx, wy, wz
+    qx = Nut_q_current_body.x
+    qy = Nut_q_current_body.y
+    qz = Nut_q_current_body.z
+    qw = Nut_q_current_body.w
+    wx = Nut_w_current_body.x
+    wy = Nut_w_current_body.y
+    wz = Nut_w_current_body.z
 
-const Nut_q_dot = new THREE.Quaternion()
-Nut_q_dot.x = 0.5*(wx*qw + wz*qy - wy*qz)
-Nut_q_dot.y = 0.5*(wy*qw - wz*qx + wx*qz)
-Nut_q_dot.z = 0.5*(wz*qw + wy*qx - wx*qy)
-Nut_q_dot.w = 0.5*(-1*wx*qx - wy*qy - wz*qz)
+    const Nut_q_dot = new THREE.Quaternion()
+    Nut_q_dot.x = 0.5*(wx*qw + wz*qy - wy*qz)
+    Nut_q_dot.y = 0.5*(wy*qw - wz*qx + wx*qz)
+    Nut_q_dot.z = 0.5*(wz*qw + wy*qx - wx*qy)
+    Nut_q_dot.w = 0.5*(-1*wx*qx - wy*qy - wz*qz)
 
-const Nut_q_next_body = new THREE.Quaternion()
-Nut_q_next_body.x = Nut_q_current_body.x + htime*Nut_q_dot.x
-Nut_q_next_body.y = Nut_q_current_body.y + htime*Nut_q_dot.y
-Nut_q_next_body.z = Nut_q_current_body.z + htime*Nut_q_dot.z
-Nut_q_next_body.w = Nut_q_current_body.w + htime*Nut_q_dot.w
-Nut_q_next_body.normalize()
+    const Nut_q_next_body = new THREE.Quaternion()
+    Nut_q_next_body.x = Nut_q_current_body.x + htime*Nut_q_dot.x
+    Nut_q_next_body.y = Nut_q_current_body.y + htime*Nut_q_dot.y
+    Nut_q_next_body.z = Nut_q_current_body.z + htime*Nut_q_dot.z
+    Nut_q_next_body.w = Nut_q_current_body.w + htime*Nut_q_dot.w
+    Nut_q_next_body.normalize()
 
-const Nut_w_next_body = new THREE.Vector3()
-Nut_w_next_body.x = Nut_w_current_body.x + htime*Nut_w_dot_current_body.x
-Nut_w_next_body.y = Nut_w_current_body.y + htime*Nut_w_dot_current_body.y
-Nut_w_next_body.z = Nut_w_current_body.z + htime*Nut_w_dot_current_body.z
+    const Nut_w_next_body = new THREE.Vector3()
+    Nut_w_next_body.x = Nut_w_current_body.x + htime*Nut_w_dot_current_body.x
+    Nut_w_next_body.y = Nut_w_current_body.y + htime*Nut_w_dot_current_body.y
+    Nut_w_next_body.z = Nut_w_current_body.z + htime*Nut_w_dot_current_body.z
 
+    const Nut_q_current_body_inv = new THREE.Quaternion()
+    Nut_q_current_body_inv.copy(Nut_q_current_body)
+    Nut_q_current_body_inv.invert()
 
+    const Nut_q_diff_body = new THREE.Quaternion()
+    Nut_q_diff_body.copy(Nut_q_next_body)
+    Nut_q_diff_body.multiply(Nut_q_current_body_inv)
+
+    const Nut_q_next_world = new THREE.Quaternion()
+    Nut_q_next_world.copy(Nut_q_diff_body)
+    Nut_q_next_world.multiply(Nut_q_current_world)
+
+    const Nut_w_next_world = new THREE.Vector3()
+    Nut_w_next_world.copy(Nut_w_next_body)
+    Nut_w_next_world.applyQuaternion(Nut_q_current_world)
+
+    //Step forward ==========================
+    Nut_q_current_world.copy(Nut_q_next_world)
+    const Nut_q_current_world_inv = new THREE.Quaternion()
+    Nut_q_current_world_inv.copy(Nut_q_current_world)
+    Nut_q_current_world_inv.invert()
+    Nut_q_current_body.set(0, 0, 0, 1)
+    Nut_w_current_world.copy(Nut_w_next_world)
+    Nut_w_current_body.copy(Nut_w_current_world)
+    Nut_w_current_body.applyQuaternion(Nut_q_current_world_inv)
+}
 
 //Sizes
 const sizes = {
@@ -108,10 +141,10 @@ const sizes = {
 }
 
 //Light
-const light1 = new THREE.PointLight(0xffffff, 1, 200)
+const light1 = new THREE.PointLight(0xffffff, 5, 200)
 light1.position.set(0, 50, 50)
 scene.add(light1)
-const light2 = new THREE.PointLight(0xffffff, 1, 200)
+const light2 = new THREE.PointLight(0xffffff, 5, 200)
 light2.position.set(0, -50, -50)
 scene.add(light2)
 
@@ -144,7 +177,7 @@ window.addEventListener('resize', () => {
 var frameCount = 0
 var fpsInterval, startTime, now, then, elapsed
 
-startLoopAtFPS(30) // max FPS is 60
+startLoopAtFPS(60) // max FPS is 60
 
 function startLoopAtFPS(fps) {
     fpsInterval = 1000 / fps
@@ -168,9 +201,11 @@ function loop() {
 
         // draw animating objects here...
         if (model_nut) {
-            const qrot1 = new THREE.Quaternion()
-            qrot1.setFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), Math.PI / 200)
-            model_nut.scene.applyQuaternion(qrot1)
+            for (let i = 0; i < 10; i++) {
+                update_Nut_pose()
+            }
+            model_nut.scene.setRotationFromQuaternion(Nut_q_current_world)
+            axesBody.setRotationFromQuaternion(Nut_q_current_world)
         }
         control.update()
         renderer.render(scene, camera)
