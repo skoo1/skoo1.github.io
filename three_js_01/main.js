325ecccc826f450610d9bd087b55a53a44267733
Musculoskeletal BioDynamics Lab @ KAIST <43591202+skoo1@users.noreply.github.com>
2023-03-26 09:41:04 +0900

    Update main.js

--- three_js_01/main.js
+++ three_js_01/main.js
@@ -32,6 +32,75 @@ loader.load(
     }
 )
 
+//Dynamics Calculations
+const htime = 0.01
+const NutIB = new THREE.Matrix3()
+NutIB.set(1, 0, 0,
+          0, 2, 0,
+          0, 0, 3)
+
+const Nutq0_world = new THREE.Quaternion()
+Nutq0_world.set(0, 0, 0, 1)
+const Nutw0_body = new THREE.Vector3()
+Nutw0_body.set(0, 0, 1)
+const NutTorque_body = new THREE.Vector3()
+NutTorque_body.set(0, 0, 0)
+
+const Nut_q_current_world = new THREE.Quaternion()
+Nut_q_current_world.copy(Nutq0_world)
+const Nut_q_current_body = new THREE.Quaternion()
+Nut_q_current_body.set(0, 0, 0, 1)
+const Nut_w_current_body = new THREE.Vector3()
+Nut_w_current_body.copy(Nutw0_body)
+
+// calculate w_croxx_IB_mult_w
+const Nut_Iw_current_body = new THREE.Vector3()
+Nut_Iw_current_body.copy(Nut_w_current_body)
+Nut_Iw_current_body.applyMatrix3(NutIB)
+const Nut_w_x_Iw_current_body = new THREE.Vector3()
+Nut_w_x_Iw_current_body.copy(Nut_w_current_body)
+Nut_w_x_Iw_current_body.cross(Nut_Iw_current_body)
+
+// calculate w_dot_body = IBInv * (Torque - w_croxx_IB_mult_w)
+const Nut_Torque_sub_wIw = new THREE.Vector3()
+Nut_Torque_sub_wIw.copy(NutTorque_body)
+Nut_Torque_sub_wIw.sub(Nut_w_x_Iw_current_body)
+const NutIBInv = new THREE.Matrix3()
+NutIBInv.copy(NutIB)
+NutIBInv.invert()
+const Nut_w_dot_current_body = new THREE.Vector3()
+Nut_w_dot_current_body.copy(Nut_Torque_sub_wIw)
+Nut_w_dot_current_body.applyMatrix3(NutIBInv)
+
+var qx, qy, qz, qw, wx, wy, wz
+qx = Nut_q_current_body.x
+qy = Nut_q_current_body.y
+qz = Nut_q_current_body.z
+qw = Nut_q_current_body.w
+wx = Nut_w_current_body.x
+wy = Nut_w_current_body.y
+wz = Nut_w_current_body.z
+
+const Nut_q_dot = new THREE.Quaternion()
+Nut_q_dot.x = 0.5*(wx*qw + wz*qy - wy*qz)
+Nut_q_dot.y = 0.5*(wy*qw - wz*qx + wx*qz)
+Nut_q_dot.z = 0.5*(wz*qw + wy*qx - wx*qy)
+Nut_q_dot.w = 0.5*(-1*wx*qx - wy*qy - wz*qz)
+
+const Nut_q_next_body = new THREE.Quaternion()
+Nut_q_next_body.x = Nut_q_current_body.x + htime*Nut_q_dot.x
+Nut_q_next_body.y = Nut_q_current_body.y + htime*Nut_q_dot.y
+Nut_q_next_body.z = Nut_q_current_body.z + htime*Nut_q_dot.z
+Nut_q_next_body.w = Nut_q_current_body.w + htime*Nut_q_dot.w
+Nut_q_next_body.normalize()
+
+const Nut_w_next_body = new THREE.Vector3()
+Nut_w_next_body.x = Nut_w_current_body.x + htime*Nut_w_dot_current_body.x
+Nut_w_next_body.y = Nut_w_current_body.y + htime*Nut_w_dot_current_body.y
+Nut_w_next_body.z = Nut_w_current_body.z + htime*Nut_w_dot_current_body.z
+
+
+
 //Sizes
 const sizes = {
     width: window.innerWidth,
@@ -112,3 +181,5 @@ function loop() {
         // console.log(currentFps)
     }
 }
+
+
